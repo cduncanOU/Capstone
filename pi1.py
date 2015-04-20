aa7e33e78d05f34d002e2262796b02032f7446eb
Cooper <c.duncan@ou.edu>
2015-04-20 10:40:18 -0500

    Update pi1.py

--- pi1.py
+++ pi1.py
@@ -15,19 +15,24 @@ BAUD_RATE = 9600
 
 mode_pin = 17 # Pin 11 on Pi. Used for Texas/Oklahoma selection
 ready_pin = 2 # Pin 3 on Pi. Used to tell when 2nd Pi is ready
+debug_led = 18 # Pin 12 on Pi. Used for debugging
 
+# Returns a string value to send to the 2nd Pi
+# "t\nweightvalue\nheadvalue\navgvalue where t is for Texas output (replace with o for Oklahoma)
 def transfer_string(b,w,h,a):
 	s = ""
 	if b == True:
 		s = "o"
 	else:
-		s = "t"
-	s = s + str(w) + "!"
-	s = s + str(h) + "$"
+		s = "t\n"
+	s = s + str(w) + "\n"
+	s = s + str(h) + "\n"
 	s = s + str(a) + "\n"
-	print s
+	#print s # Used for debugging
 	return s
 	
+#Check Oklahoma/Texas mode switch
+#Return True if Oklahoma mode	
 def check_mode():
 	if GPIO.input(mode_pin):
 		return True
@@ -35,6 +40,7 @@ def check_mode():
 		return False
 	return
 
+# Display values based on Oklahoma or Texas (replace with pygame code)
 def update_display(b,w,h,a):
 	if b:
 		print "WT: " + str(w)
@@ -55,19 +61,27 @@ def main():
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(mode_pin, GPIO.IN)
 	GPIO.setup(ready_pin, GPIO.IN)
+	GPIO.setup(debug_led, GPIO.OUT)
 	
-	while GPIO.input(ready_pin) == 0:
-		time.sleep(1)
-		
+	# This will be used to detect when 2nd pi is ready
+	#while GPIO.input(ready_pin) == 0:
+	#	time.sleep(1)
+	
+	# Start the serial connection	
 	uart = serial.Serial("/dev/ttyAMA0", baudrate=BAUD_RATE, timeout=TIMEOUT)
 	uart.open()
 	
-	b_Oklahoma = check_mode()
+	GPIO.output(debug_led, True) # Signal that the program has started
+	
+	b_Oklahoma = check_mode() # True if mode is Oklahoma, false if Texas
+	
+	# First update of display and write zero values to 2nd Pi
 	update_display(b_Oklahoma, weight_current, head_current, avg_current)
 	uart.write(transfer_string(b_Oklahoma, weight_prev, head_prev, avg_prev))
 	
 	while True:
 		mode = check_mode()
+		#If the mode has changed update both displays
 		if b_Oklahoma != mode :
 			b_Oklahoma = mode
 			update_display(b_Oklahoma, weight_current, head_current, avg_current)
@@ -78,7 +92,7 @@ def main():
 		if input != '':
 			if 'a' in input:
 				input = uart.readline()
-				input = input[0:len(input)-1]
+				input = input[0:len(input)-2]
 				weight_prev = weight_current
 				weight_current = float(input)
 				update_display(b_Oklahoma, weight_current, head_current, avg_current)
@@ -86,7 +100,7 @@ def main():
 			
 			elif 'b' in input:
 				input = uart.readline()
-				input = input[0:len(input)-1]
+				input = input[0:len(input)-2]
 				head_prev = head_current
 				head_current = int(input)
 				input = uart.readline()
